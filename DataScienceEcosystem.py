7d5af34f44471f96fcb1d256aeb6d26c96ac2808
A7MSDS <162021545+A7MSDS@users.noreply.github.com>
2024-03-04 13:15:58 +0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# *This notebook is for final exam in the data science tools cource, it is about the Data Science Tools and Ecosystem.*
+
+# Objectives:
+# 1. Popular luanguages in data science 
+# 2. Common libraries
+# 3. Data science tools
+
+# **Some of the popular languages that Data Scientists use are:**
+# 
+
+# - Python
+# - Java script
+# - R
+# - C++
+
+# **Some of the commonly used libraries used by Data Scientists include:**
+
+# - Scientific Computing Libraries in Python.
+# - Visualization Libraries in Python 
+# - High-level Machine Learning and Deep learning
+# - Libaraies Use in Other languge
+
+# |Data Science Tools|
+# --------------------
+# |Jupter Lab|
+# |R|
+# |Github|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[7]:
+
+
+1+1
+
+
+# In[20]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[33]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+
+# ## Author
+# Ahmed Sadig Ali Abdalla
